14cadfbac1f9dbb02b1ac9ed92bd48127a6f447b
ldfxxx404 <71141603+ldfxxx404@users.noreply.github.com>
2024-12-30 13:59:35 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 # servers config
 UPLOAD_FOLDER = 'Storage'
-UPLOAD_FOLDER = '/home/lxmxr/main_server/home_server/Storage'
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'webm', 'mp4', 'mp3', 'png', 'txt'}
 IP_ADDRESS = '0.0.0.0'
 PORT = '1337'
