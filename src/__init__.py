bfe547d500347904f7c489a38daf6312190b0e59
lnkssr <gifzoner5@gmail.com>
2024-12-31 09:53:42 +0300

    add db auth

--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,31 @@
-import os
 from flask import Flask
-from .config import UPLOAD_FOLDER, IP_ADDRESS, PORT
-from .auth import auth as auth_blueprint
-from .main import main as main_blueprint
+from flask_sqlalchemy import SQLAlchemy
+from .config import Config
+import os
+
+# Инициализация базы данных
+db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates')
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'webm', 'mp4', 'mp3', 'png', 'txt'}
+    app = Flask(__name__, instance_relative_config=True, template_folder='../templates')  # Укажите путь к шаблонам
+    
+    # Настройка конфигурации приложения
+    app.config.from_object(Config)  # Загружаем конфигурацию из config.py
+    app.secret_key = Config.SECRET_KEY 
+    # Инициализация базы данных с приложением
+    db.init_app(app)
 
-    # Register blueprints
+    # Регистрация блюпринтов
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
 
-    # Create upload folder if it doesn't exist
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
+    # Создание папки для загрузок, если она не существует
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    with app.app_context():
+        db.create_all()  # Создание всех таблиц
 
     return app
--- src/auth.py
+++ src/auth.py
@@ -1,15 +1,49 @@
-from flask import Blueprint, render_template
+import os
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User  # Импортируем модель User
+from . import db  # Импортируем объект db
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def profile():
-    return render_template('login.html') 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-@auth.route('/signup')
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            session['user'] = user.email
+            return redirect(url_for('main.home'))
+        else:
+            flash('Invalid email or password', 'error')
+            return render_template('login.html')
+
+    return render_template('login.html')
+
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html') 
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        name = request.form.get('name')
+
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('Email address already exists. Please choose a different one.', 'error')
+            return render_template('signup.html')
+
+        new_user = User(email=email, password=generate_password_hash(password), name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful! You can now log in.', 'success')
+        return redirect(url_for('auth.login'))
+
+    return render_template('signup.html')
 
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+    session.pop('user', None)
+    return redirect(url_for('auth.login'))
