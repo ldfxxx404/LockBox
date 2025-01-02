b164d73653ccbbd0728c1e1dd66f759918bccc34
lnkssr <gifzoner5@gmail.com>
2025-01-02 09:35:36 +0300

    ftm

--- src/main.py
+++ src/main.py
@@ -3,21 +3,17 @@ from flask import Blueprint, render_template, request, send_from_directory, abor
 from werkzeug.utils import secure_filename
 from urllib.parse import unquote
 
-# Assuming this is imported correctly
 from .config import *
 
 main = Blueprint('main', __name__)
 
-# Function to check allowed file extensions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in Config.ALLOWED_EXTENSIONS
-
-# Home route
+    
 @main.route('/')
 def home():
     return render_template('index.html')
 
-# Successful file upload route
 @main.route('/success', methods=['POST'])
 def success():
     if 'file' not in request.files:
@@ -37,11 +33,9 @@ def success():
 
 @main.route('/storage')
 def storage():
-    # Use current_app to get the config
     files = os.listdir(current_app.config['UPLOAD_FOLDER'])
     return render_template('storage.html', files=files)
 
-# File download route
 @main.route('/storage/<path:filename>')
 def download_file(filename):
     filename = unquote(filename)
