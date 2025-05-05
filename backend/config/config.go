22495c36bf5595f6fda4b53acb8010a7859b897e
lnkssr <gifzoner5@gmail.com>
2025-05-05 18:25:28 +0300

    chore: rewite getEnv func

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,9 +15,9 @@ var (
 	JWTSecret     = getEnv("JWT_SECRET", "supersecretkey")
 	StorageDir    = getEnv("STORAGE_DIR", "./storage")
 	PostgresLink  = getEnv("POSTGRES_URL", "postgres://postgres:postgres@localhost:6969/lock_box?sslmode=disable")
-	StorageLimit  = 20
-	MigrationsDir = "migrations"
-	DbName        = "postgres"
+	MigrationsDir = getEnv("MIGRATIONS_DIR", "migrations")
+	DbName        = getEnv("DB_NAME", "postgres")
+	StorageLimit  = getEnv("STORAGE_LIMIT", 10)
 	Limiter       = limiter.Config{
 		Expiration: 10 * time.Second,
 		Max:        3,
@@ -25,9 +27,37 @@ var (
 	}
 )
 
-func getEnv(key, fallback string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
+func getEnv[T any](key string, fallback T) T {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	switch any(fallback).(type) {
+	case string:
+		return any(val).(T)
+	case int:
+		i, err := strconv.Atoi(val)
+		if err == nil {
+			return any(i).(T)
+		}
+	case float64:
+		f, err := strconv.ParseFloat(val, 64)
+		if err == nil {
+			return any(f).(T)
+		}
+	case bool:
+		b, err := strconv.ParseBool(val)
+		if err == nil {
+			return any(b).(T)
+		}
+	case time.Duration:
+		d, err := time.ParseDuration(val)
+		if err == nil {
+			return any(d).(T)
+		}
+	default:
+		fmt.Printf("Unsupported type for key %s\n", key)
 	}
 	return fallback
 }
