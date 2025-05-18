99095add23e48b61d0a9e1582ade367c31ba455c
lnkssr <gifzoner5@gmail.com>
2025-05-18 11:29:21 +0300

    chore_fix: add automatic docs generate, fix errors messages

--- backend/internal/middleware/auth.go
+++ backend/internal/middleware/auth.go
@@ -4,51 +4,53 @@ import (
 	"back/config"
 	"back/internal/models"
 
+	"back/internal/models"
+
 	"github.com/gofiber/fiber/v2"
 	jwtware "github.com/gofiber/jwt/v3"
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func AuthRequired() fiber.Handler {
-	return jwtware.New(jwtware.Config{
-		SigningKey:   []byte(config.JWTSecret),
-		ContextKey:   "user",
-		ErrorHandler: jwtError,
-	})
+    return jwtware.New(jwtware.Config{
+        SigningKey:   []byte(config.JWTSecret),
+        ContextKey:   "user",
+        ErrorHandler: jwtError,
+    })
 }
 
 func jwtError(c *fiber.Ctx, err error) error {
-	return c.Status(fiber.StatusUnauthorized).JSON(models.ErrorResponse{
-		Message: "Unauthorized",
-		Error:   err.Error(),
-	})
+    return c.Status(fiber.StatusUnauthorized).JSON(models.ErrorResponse{
+        Message: "Unauthorized",
+        Error:   err.Error(),
+    })
 }
 
 func unauthorizedResponse(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusUnauthorized).JSON(models.ErrorResponse{
-		Message: "Unauthorized",
-		Error:   "access denied",
-	})
+    return c.Status(fiber.StatusUnauthorized).JSON(models.ErrorResponse{
+        Message: "Unauthorized",
+        Error:   "access denied",
+    })
 }
 
 func AdminRequired() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		userToken := c.Locals("user")
-		if userToken == nil {
-			return unauthorizedResponse(c)
-		}
-		token, ok := userToken.(*jwt.Token)
-		if !ok {
-			return unauthorizedResponse(c)
-		}
-		claims, ok := token.Claims.(jwt.MapClaims)
-		if !ok {
-			return unauthorizedResponse(c)
-		}
-		isAdmin, ok := claims["is_admin"].(bool)
-		if !ok || !isAdmin {
-			return unauthorizedResponse(c)
-		}
-		return c.Next()
-	}
+    return func(c *fiber.Ctx) error {
+        userToken := c.Locals("user")
+        if userToken == nil {
+            return unauthorizedResponse(c)
+        }
+        token, ok := userToken.(*jwt.Token)
+        if !ok {
+            return unauthorizedResponse(c)
+        }
+        claims, ok := token.Claims.(jwt.MapClaims)
+        if !ok {
+            return unauthorizedResponse(c)
+        }
+        isAdmin, ok := claims["is_admin"].(bool)
+        if !ok || !isAdmin {
+            return unauthorizedResponse(c)
+        }
+        return c.Next()
+    }
 }
