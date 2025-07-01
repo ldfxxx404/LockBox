d670a57b7a69dd7c0c33bfbd2a1a17ae05b22ef2
ldfxxx404 <limur9990@gmail.com>
2025-07-01 11:42:11 +0300

    feat: remove old frontend and add new

--- frontend/src/config/env.ts
+++
@@ -1,7 +0,0 @@
-interface Environment {
-  NEXT_PUBLIC_API_URL: string
-}
-
-export const Env: Environment = {
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL as string,
-}
--- frontend/src/utils/apiUrl.tsx
+++
@@ -1,3 +0,0 @@
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-export const SERVER_API =
-  process.env.NEXT_PUBLIC_SERVER || 'http://localhost:5000'
