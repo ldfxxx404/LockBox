9461ec2263aa9e4b20e40c2673f5b58eb25e1722
ldfxxx404 <limur9990@gmail.com>
2025-07-01 16:59:20 +0300

    feat: add API endpoints for authentication and registration

---
+++ frontend/src/app/constants/api.ts
@@ -0,0 +1,3 @@
+export const NEXT_PUBLIC_API_BASE_URL = 'http://localhost:5000/api';
+export const REGISTER_URL = `${NEXT_PUBLIC_API_BASE_URL}/register`;
+export const LOGIN_URL = `${NEXT_PUBLIC_API_BASE_URL}/login`;
