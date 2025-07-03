07ae0dd1944292ac2a45c98a517adcf3fa0ef9ec
ldfxxx404 <limur9990@gmail.com>
2025-07-03 14:03:00 +0300

    feat: Add UserProfile component with user storage information display

---
+++ frontend/src/app/user_profile/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+export default function UserProfile() {
+  return (
+    <div className='min-h-screen bg-[#232536] flex flex-col items-center py-10'>
+      <div className='flex items-center gap-6 bg-[#2d2f44] px-8 py-6 rounded-xl shadow-lg w-full max-w-2xl border-b border-[#35364a]'>
+        <div className='flex-shrink-0'>
+          <div className='w-20 h-20 rounded-full bg-[#35364a] border-4 border-indigo-400 flex items-center justify-center text-3xl font-bold text-indigo-300'>
+            AA
+          </div>
+        </div>
+        <div>
+          <h2 className='text-2xl font-semibold text-white mb-1'>A A</h2>
+          <p className='text-[#b0b3c6]'>a@a.a</p>
+        </div>
+      </div>
+      <div className='bg-[#2d2f44] mt-8 px-8 py-6 rounded-xl shadow-lg w-full max-w-2xl'>
+        <h3 className='text-indigo-400 text-lg font-semibold mb-4'>
+          User storage information
+        </h3>
+        <ul className='space-y-3 text-white'>
+          <li>
+            <span className='text-[#b0b3c6]'>Storage usage:</span> 24 MiB
+          </li>
+          <li>
+            <span className='text-[#b0b3c6]'>Free space:</span> 1000 MiB
+          </li>
+        </ul>
+        <div className='justify-center flex gap-5 mt-8 '>
+          <button className='bg-indigo-500 hover:bg-indigo-600 transition text-white font-semibold py-2 px-6 rounded-lg'>
+            Upload
+          </button>
+          <button className='bg-indigo-500 hover:bg-indigo-600 transition text-white font-semibold py-2 px-6 rounded-lg'>
+            Download
+          </button>
+          <button className='bg-indigo-500 hover:bg-indigo-600 transition text-white font-semibold py-2 px-6 rounded-lg'>
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
