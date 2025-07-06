639c7de9c65db43898f6b2b547b004cf0fd73a6e
ldfxxx404 <limur9990@gmail.com>
2025-07-06 10:22:39 +0300

    ci: fix prerender error for /profile page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,21 +1,33 @@
 'use client'
+import { use, useEffect, useState} from "react"  
 
 export default function UserProfile() {
+
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+
+useEffect(() => {
   const name = localStorage.getItem('name')
   const email = localStorage.getItem('email')
+
+  if (name && email) {
+    setName(name) 
+    setEmail(email)
+  }
+
+}, [])
 
   return (
     <div className='min-h-screen bg-[#232536] flex flex-col items-center py-10'>
       <div className='flex items-center gap-6 bg-[#2d2f44] px-8 py-6 rounded-xl shadow-lg w-full max-w-2xl border-b border-[#35364a]'>
         <div className='flex-shrink-0'>
           <div className='w-20 h-20 rounded-full bg-[#35364a] border-4 border-indigo-400 flex items-center justify-center text-3xl font-bold text-indigo-300'>
-            LB {/*имплементировать получение имени пользователя*/}
+            LB 
           </div>
         </div>
         <div>
           <h2 className='text-2xl font-semibold text-white mb-1'>{name}</h2>
           <p className='text-[#b0b3c6]'>{email}</p>{' '}
-          {/* Реализовать получение почты текущего пользователя*/}
         </div>
       </div>
       <div className='bg-[#2d2f44] mt-8 px-8 py-6 rounded-xl shadow-lg w-full max-w-2xl'>
@@ -25,11 +37,9 @@ export default function UserProfile() {
         <ul className='space-y-3 text-white'>
           <li>
             <span className='text-[#b0b3c6]'>Storage usage:</span> 24 MiB{' '}
-            {/* Имплементировать получение кол-ва занятой памяти*/}
           </li>
           <li>
             <span className='text-[#b0b3c6]'>Free space:</span> 1000 MiB{' '}
-            {/* Имплементировать получение кол-ва свободной памяти*/}
           </li>
         </ul>
         <div className='justify-center flex gap-5 mt-8 '>
