b464b971c1ac6db0c55d4e176e46e4a37ae64fb1
lnkssr <gifzoner5@gmail.com>
2025-07-07 03:12:24 +0300

    chore: final fix logging

--- backend/internal/services/file.go
+++ backend/internal/services/file.go
@@ -60,101 +60,129 @@ func NewFileService(
 }
 
 func (s *FileService) UploadFile(userID int, fileHeader *multipart.FileHeader) error {
+	log.Infof("Starting file upload: user_id=%d, filename=%s, size=%d", userID, fileHeader.Filename, fileHeader.Size)
+
 	file, err := fileHeader.Open()
 	if err != nil {
-		log.Error("open file header", err)
+		log.Error("Failed to open file header:", err)
 		return err
 	}
 	defer func() {
-		err = file.Close()
-		if err != nil {
-			log.Error("file close error", err)
+		if cerr := file.Close(); cerr != nil {
+			log.Error("Failed to close file:", cerr)
 		}
 	}()
 
 	objectName := fmt.Sprintf("%d/%s", userID, fileHeader.Filename)
-	size := fileHeader.Size
 	contentType := fileHeader.Header.Get("Content-Type")
 
 	uploadInfo, err := s.Minio.PutObject(context.Background(),
 		s.Bucket,
 		objectName,
 		file,
-		size,
+		fileHeader.Size,
 		minio.PutObjectOptions{
 			ContentType: contentType,
 		})
-	log.Debug("put object minio:", "upload file info", uploadInfo)
 	if err != nil {
-		log.Error("put object error", err)
+		log.Error("MinIO PutObject error:", err)
 		return err
 	}
+	log.Infof("File uploaded to MinIO: location=%s, etag=%s", uploadInfo.Location, uploadInfo.ETag)
 
 	meta := &models.File{
 		UserID:       userID,
 		Filename:     fileHeader.Filename,
 		OriginalName: fileHeader.Filename,
-		Size:         size,
+		Size:         fileHeader.Size,
 		MimeType:     contentType,
 	}
-	return s.FileRepo.Create(meta)
+	if err := s.FileRepo.Create(meta); err != nil {
+		log.Error("Failed to save file metadata to DB:", err)
+		return err
+	}
+	log.Infof("File metadata saved: user_id=%d, filename=%s", userID, fileHeader.Filename)
+
+	return nil
 }
 
 func (s *FileService) ListFiles(userID int) ([]models.File, error) {
-	return s.FileRepo.GetFilesByUser(userID)
+	log.Infof("Listing files for user_id=%d", userID)
+	files, err := s.FileRepo.GetFilesByUser(userID)
+	if err != nil {
+		log.Error("Failed to list files:", err)
+		return nil, err
+	}
+	log.Infof("Found %d files for user_id=%d", len(files), userID)
+	return files, nil
 }
 
 func (s *FileService) GetFile(userID int, filename string) ([]byte, error) {
 	objectName := fmt.Sprintf("%d/%s", userID, filename)
+	log.Infof("Fetching file: user_id=%d, filename=%s", userID, filename)
 
 	obj, err := s.Minio.GetObject(context.Background(),
 		s.Bucket,
 		objectName,
 		minio.GetObjectOptions{})
 	if err != nil {
+		log.Error("Failed to get object from MinIO:", err)
 		return nil, fmt.Errorf("get object error: %w", err)
 	}
-
 	defer func() { _ = obj.Close() }()
+
 	data, err := io.ReadAll(obj)
 	if err != nil {
+		log.Error("Failed to read object data:", err)
 		return nil, fmt.Errorf("failed to read object: %w", err)
 	}
-
+	log.Infof("File read successfully: user_id=%d, filename=%s, size=%d", userID, filename, len(data))
 	return data, nil
 }
 
 func (s *FileService) DeleteFile(userID int, filename string) error {
 	objectName := fmt.Sprintf("%d/%s", userID, filename)
-	err := s.Minio.RemoveObject(context.Background(),
+	log.Infof("Deleting file: user_id=%d, filename=%s", userID, filename)
+
+	if err := s.Minio.RemoveObject(context.Background(),
 		s.Bucket,
 		objectName,
-		minio.RemoveObjectOptions{})
-	if err != nil {
-		log.Error("error of delete file", err)
+		minio.RemoveObjectOptions{}); err != nil {
+		log.Error("Failed to delete file from MinIO:", err)
 		return errors.New("failed to delete file")
 	}
-	return s.FileRepo.DeleteFile(userID, filename)
+	if err := s.FileRepo.DeleteFile(userID, filename); err != nil {
+		log.Error("Failed to delete file from DB:", err)
+		return err
+	}
+	log.Infof("File deleted successfully: user_id=%d, filename=%s", userID, filename)
+	return nil
 }
 
 func (s *FileService) GetStorageInfo(userID int) (usedMB int64, limitMB int, err error) {
+	log.Infof("Fetching storage info for user_id=%d", userID)
+
 	prefix := fmt.Sprintf("%d/", userID)
 	opts := minio.ListObjectsOptions{Prefix: prefix, Recursive: true}
 
 	var totalSize int64
 	for object := range s.Minio.ListObjects(context.Background(), s.Bucket, opts) {
 		if object.Err != nil {
+			log.Error("Failed to list object:", object.Err)
 			return 0, 0, object.Err
 		}
 		totalSize += object.Size
 	}
 	usedMB = totalSize / (1024 * 1024)
+	log.Infof("Used storage: user_id=%d, usedMB=%d", userID, usedMB)
 
 	user, err := s.UserRepo.GetByID(userID)
 	if err != nil {
-		log.Error("error of use repo", err)
+		log.Error("Failed to fetch user data:", err)
 		return 0, 0, err
 	}
 	limitMB = user.StorageLimit
+	log.Infof("Storage info: user_id=%d, limitMB=%d", userID, limitMB)
+
 	return usedMB, limitMB, nil
 }
