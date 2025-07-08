a454212cb9929b7e806b789caf25ed0b567b9ff1
ldfxxx404 <limur9990@gmail.com>
2025-07-09 00:50:11 +0300

    fix: prettiter format

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function UserProfile() {
-const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true)
   const router = useRouter()
 
   const handleLogout = () => {
