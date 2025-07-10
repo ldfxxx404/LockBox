db33d1f308c08683e60295db38dcddbe88f2c134
ldfxxx404 <limur9990@gmail.com>
2025-07-10 16:04:38 +0300

    feat: style 404 page

--- frontend/src/app/404/page.tsx
+++ frontend/src/app/404/page.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
-    <div className='items-center justify-center'>
-      404
-      {/* <img src="/images/404.jpg" className="w-[900px] h-[910px] mx-auto block mt-[1px]"/> */}
+    <div className='items-center justify-center flex flex-col mt-56'>
+      <h1 className='text-9xl text-[#ff5555]'>404</h1>
+      <h2 className='text-4xl text-[#ffb86c]'>Page not found</h2>
     </div>
   )
 }
