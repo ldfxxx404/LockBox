f95ff2fe3f3a06680b8e9b0f0d731a361aa843bf
ldfxxx404 <limur9990@gmail.com>
2025-07-13 12:57:14 +0300

    fix: prettier

--- frontend/src/app/lib/clientLogout.ts
+++ frontend/src/app/lib/clientLogout.ts
@@ -14,7 +14,7 @@ export default async function logout(): Promise<LogoutResponse | null> {
       },
     })
 
-      if (res.ok) {
+    if (res.ok) {
       return { message: 'Logout succesfully' }
     }
 
