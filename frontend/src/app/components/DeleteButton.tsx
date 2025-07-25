33e231f4df581129a5ae6e22ff618c074ca94f23
ldfxxx404 <limur9990@gmail.com>
2025-07-25 19:42:21 +0300

    feat: style delete button

--- frontend/src/app/components/DeleteButton.tsx
+++ frontend/src/app/components/DeleteButton.tsx
@@ -23,7 +23,7 @@ export const DeleteButton = ({ filename, onDelete }: DeleteButtonProps) => {
   return (
     <button
       onClick={handleClick}
-      className='text-red-500 hover:text-red-700 hover:underline ml-4 whitespace-nowrap'
+      className='text-red-500 hover:text-red-700 rounded-lg border mt-1.5 whitespace-nowrap ml-4 mr-4 pr-2 pl-2 '
     >
       Delete
     </button>
