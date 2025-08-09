41c437f629b933664007d11ee671eb3403865f3e
ldfxxx404 <limur9990@gmail.com>
2025-08-09 12:15:14 +0300

    fix: import

--- frontend/src/app/api/admin/admins/route.ts
+++ frontend/src/app/api/admin/admins/route.ts
@@ -1,4 +1,4 @@
-import { GET_ADMINS_URL, GET_USERS_URL } from '@/constants/api'
+import { GET_ADMINS_URL } from '@/constants/api'
 import { clogger } from '@/utils/ColorLogger'
 import { NextResponse } from 'next/server'
 import { ErrorResponse } from '@/types/errorResponse'
