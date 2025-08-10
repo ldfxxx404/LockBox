aa84cc52569642878cec59bc1b295a7778f75cc4
ldfxxx404 <limur9990@gmail.com>
2025-08-10 10:36:29 +0300

    fix: prettier fmt

--- frontend/src/app/404/page.tsx
+++ frontend/src/app/404/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { Button } from "@/components/ActionButton"
-import { useRouter } from "next/navigation"
+import { Button } from '@/components/ActionButton'
+import { useRouter } from 'next/navigation'
 
 export default function NotFound() {
   const router = useRouter()
@@ -9,8 +9,12 @@ export default function NotFound() {
     <div className='items-center justify-center flex flex-col mt-56'>
       <h1 className='text-9xl text-[#ff5555]'>404</h1>
       <h2 className='text-4xl text-[#ffb86c]'>Page not found</h2>
-      <div className="pt-96">
-      <Button label="Go back" type="submit" onClick={() => router.push('/')} />
+      <div className='pt-96'>
+        <Button
+          label='Go back'
+          type='submit'
+          onClick={() => router.push('/')}
+        />
       </div>
     </div>
   )
