47cfb17b82badbe4fe052e46ba13652414380bad
ldfxxx404 <71141603+ldfxxx404@users.noreply.github.com>
2025-08-23 10:28:28 +0300

    Add GetData function for user profile retrieval with error handling

---
+++ frontend/src/utils/GetUserData.ts
@@ -0,0 +1,47 @@
+import { PROFILE_URL } from '@/constants/api'
+import { clogger } from './ColorLogger'
+import { ErrorResponse } from '@/types/errorResponse'
+import { NextResponse } from 'next/server'
+
+export async function GetData(req: Request) {
+  try {
+    const authHeader = req.headers.get('authorization')
+    const token = authHeader?.split(' ')[1]
+    if (!token) {
+      const error: ErrorResponse = {
+        message: 'Unauthorized',
+        code: 401,
+      }
+      clogger.error('Authorization token is missing for /profile request.')
+      return NextResponse.json(error, { status: error.code })
+    }
+      const res = await fetch(PROFILE_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      })
+
+      if (!res.ok) {
+        clogger.error('Error requesting profile')
+        return null
+      }
+
+      const data = await res.json()
+      const email = <string>data.user.email
+      const name = <string>data.user.name
+      const id = <number>data.user.id
+      return { email, name, id }
+
+    
+    
+  } catch (error) {
+    console.error('Profile API error:', error)
+    const response: ErrorResponse = {
+      message: 'Internal server error',
+      code: 500,
+    }
+    return NextResponse.json(response, { status: 500 })
+  }
+}
