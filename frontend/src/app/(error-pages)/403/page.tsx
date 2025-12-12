e54724bec805f6032d6305a3f9cd8feb919403a0
ldfxxx404 <limur9990@gmail.com>
2025-12-13 00:13:59 +0300

    chore(error-pages): pages with errors are combined into a single directory.

---
+++ frontend/src/app/(error-pages)/403/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Button } from '@/components/ActionButton'
+import { useRouter } from 'next/navigation'
+
+export default function NotFound() {
+  const router = useRouter()
+  return (
+    <div className='flex flex-col items-center justify-center min-h-[100dvh] w-screen overflow-hidden text-center p-4'>
+      <h1 className='text-9xl max-sm:text-6xl text-[var(--dracula-red)]'>
+        403
+      </h1>
+      <h2 className='text-4xl max-sm:text-2xl max-sm:mb-55 text-[var(--dracula-orange)] mb-8'>
+        Access denied
+      </h2>
+      <Button label='Go back' type='submit' onClick={() => router.push('/profile')}/>
+    </div>
+  )
+}
