c6c51ea28290cdaaae527d19ed700eebe652df25
ldfxxx404 <limur9990@gmail.com>
2025-12-19 14:23:20 +0300

    chore(getToken): remove try/catch block

--- frontend/src/utils/getToken.ts
+++ frontend/src/utils/getToken.ts
@@ -4,12 +4,7 @@ export const getToken = () => {
   const token = sessionStorage.getItem('token')
 
   if (token) {
-    try {
-      jwtDecode(token)
-    } catch (error) {
-      console.error('Invalid token:', error)
-      return null
-    }
+    jwtDecode(token)
   }
 
   return token
